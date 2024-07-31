9a42eac19c0f0abc806394b4839462f28b3a614a
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-07-31 12:18:11 -0700

    created sprite idle/movement animations

---
+++ Scripts/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("move_right"):
+		animated_sprite.play("run")
+	
+	if Input.is_action_just_released("move_right"):
+		animated_sprite.play("idle")
