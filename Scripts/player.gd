b6dceda377a4fea0b7c684541912fb7c50cf0e5e
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-01 13:50:22 -0700

    added animation update based on directional movements

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,11 +12,28 @@ func _physics_process(delta):
 		if velocity.y > 500:
 			velocity.y = 500
 	
-	if is_on_floor() == true:
-		if Input.is_action_just_pressed("move_jump"):
-			velocity.y = -jump_force
+	if Input.is_action_just_pressed("move_jump") && is_on_floor():
+		velocity.y = -jump_force
 	
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+		
 	velocity.x = direction * speed
 	
 	move_and_slide()
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y > 0:
+			animated_sprite.play("fall")
+		else:
+			animated_sprite.play("jump")
+	
