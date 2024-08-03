9c765e9d7658050e89cbc9735cdc09b64e8d8ca0
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-02 20:08:42 -0700

    created a death zone incase player falls, resets players position

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
+@onready var start_position = $StartPosition
 func _process(delta):
 	if Input.is_action_just_pressed("game_quit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("game_reset"):
 		get_tree().reload_current_scene()
+
+
+func _on_death_zone_body_entered(body):
+	body.velocity = Vector2.ZERO
+	body.global_position = start_position.global_position
