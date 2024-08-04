5d06a3178ddeeb6bfac0f60ab662917035b1bc72
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-04 13:21:30 -0700

    created spiked ball and saw traps

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
 @onready var start_position = $StartPosition
+
+@onready  var player = $Player
 func _process(delta):
 	if Input.is_action_just_pressed("game_quit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("game_reset"):
 		get_tree().reload_current_scene()
 
-
 func _on_death_zone_body_entered(body):
-	body.velocity = Vector2.ZERO
-	body.global_position = start_position.global_position
+	reset_player()
 
+func _on_trap_touched_player():
+	reset_player()
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start_position.global_position
