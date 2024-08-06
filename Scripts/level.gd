d07076da01fbe5a8a457c138ca04e704b82a0c81
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-06 16:42:58 -0700

    created an animated starting/respawn area

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
+@onready var start_position = $Start
 
-@onready  var player = $Player
+var player = null
 
 func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	player.global_position = start_position.get_spawn_pos()
+	
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 		trap.touched_player.connect(_on_trap_touched_player)
@@ -24,4 +27,4 @@ func _on_trap_touched_player():
 
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start_position.get_spawn_pos()
