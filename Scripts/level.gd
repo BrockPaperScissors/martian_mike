b34044547d5b347f235137289531b19eac5aa94f
BrockPaperScissors <michaeljbrock91@gmail.com>
2024-08-10 18:19:57 -0700

    created a win/victory screen

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @export var next_level: PackedScene = null
 @export var level_time = 5
+@export var is_final_level: bool = false
 
 @onready var start_position = $Start
 @onready var exit_level = $ExitArea
 @onready var death_zone = $DeathZone
 @onready var hud = $UILayer/HUD
+@onready var ui_layer = $UILayer
 
 
 var player = null
@@ -56,20 +58,26 @@ func reset_player():
 	player.global_position = start_position.get_spawn_pos()
 
 func _on_exit_body_entered(body):
+	var finished_time_left = time_left
+	
 	if body is Player:
-		if next_level != null:
+		if is_final_level || (next_level != null):
+			hud.set_time_label(finished_time_left)
 			win = true
 			exit_level.animate()
 			player.active = false
-			
 			await get_tree().create_timer(1.5).timeout
-			get_tree().change_scene_to_packed(next_level)
+			
+			if is_final_level:
+				ui_layer.show_win_screen(true)
+			else:
+				get_tree().change_scene_to_packed(next_level)
 
 func _on_level_timer_timeout():
 	time_left -= 1
 	hud.set_time_label(time_left)
 	
-	if time_left < 0:
+	if time_left < 0 && win != true:
 		reset_player()
 		time_left = level_time
 		hud.set_time_label(time_left)
---
+++ Scripts/ui_layer.gd
@@ -0,0 +1,4 @@
+extends CanvasLayer
+
+func show_win_screen(show_display: bool):
+	$WinScreen.visible = show_display
